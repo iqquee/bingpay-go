88ab52a18be6122f75e5f5f832eec99a21d40697
hisyntax <hisyntax0@gmail.com>
2022-06-24 16:28:27 +0100

    all networks implemented for airtime

--- interfaces/all_networks.go
+++ interfaces/all_networks.go
@@ -1,7 +1,33 @@
 package interfaces
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 func AllNetworks() {
-	// client := HttpClient{}
-	// url := "https://bingpay.ng/api/v1/all-networks"
-	// method := "GET"
+	client := NewHttpClient()
+	url := "https://bingpay.ng/api/v1/all-networks"
+	method := "GET"
+	token := client.Token
+	req, reqErr := http.NewRequest(method, url, nil)
+	if reqErr != nil {
+		log.Println(reqErr.Error())
+		return
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", token)
+
+	resp, respErr := client.Http.Do(req)
+	if respErr != nil {
+		log.Println(respErr.Error())
+		return
+	}
+
+	resp_body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(resp.StatusCode)
+	fmt.Println(string(resp_body))
 }
