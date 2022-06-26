c100317c8a85435e32f14d7298bdd83b88367e0f
hisyntax <hisyntax0@gmail.com>
2022-06-26 09:56:26 +0100

    all networks functions updated to return the struct of the response

--- interfaces/all_networks.go
+++ interfaces/all_networks.go
@@ -1,13 +1,26 @@
 package interfaces
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func AllNetworks() {
+type allNetworks struct {
+	Error   bool
+	Message string
+	Data    []allNetworksDataBody
+}
+
+type allNetworksDataBody struct {
+	ID   string
+	Name string
+	Note string
+}
+
+func AllNetworks() (*allNetworks, int, error) {
 	client := NewHttpClient()
 	url := "https://bingpay.ng/api/v1/all-networks"
 	method := "GET"
@@ -15,7 +28,7 @@ func AllNetworks() {
 	req, reqErr := http.NewRequest(method, url, nil)
 	if reqErr != nil {
 		log.Println(reqErr.Error())
-		return
+		return nil, 0, reqErr
 	}
 
 	req.Header.Add("Content-Type", "application/json")
@@ -24,10 +37,16 @@ func AllNetworks() {
 	resp, respErr := client.Http.Do(req)
 	if respErr != nil {
 		log.Println(respErr.Error())
-		return
+		return nil, 0, respErr
 	}
 
 	resp_body, _ := ioutil.ReadAll(resp.Body)
+	var response allNetworks
+	if err := json.Unmarshal(resp_body, &response); err != nil {
+		log.Println(err)
+		return nil, 0, err
+	}
 	fmt.Println(resp.StatusCode)
 	fmt.Println(string(resp_body))
+	return &response, resp.StatusCode, nil
 }
