522653792a5eef30c7f880d4493773d177a93e30
hisyntax <hisyntax0@gmail.com>
2022-06-27 13:42:58 +0100

    purchasebill implemented

---
+++ interfaces/utilityBills/purchase_bill.go
@@ -0,0 +1,80 @@
+package utilitybills
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/hisyntax/bingpay-go/interfaces"
+)
+
+type purchaseBill struct {
+	Service_Id  string
+	Customer_Id string
+	Variation   string
+	Amount      string
+}
+
+type purchaseBillRes struct {
+	Error          bool
+	Message        string
+	Data           purchaseBillResDataBody
+	Purchased_Code interface{}
+}
+
+type purchaseBillResDataBody struct {
+	Status          string
+	Product_Name    string
+	Unique_Element  string
+	Unit_Price      int
+	Quantity        int
+	Channel         string
+	Type            string
+	Phone           string
+	Name            interface{}
+	Convinience_fee int
+	Amount          int
+	Platform        string
+}
+
+func PurchaseBill(service_Id, customer_Id, variation, amount string) (*purchaseBillRes, int, error) {
+	client := interfaces.NewHttpClient()
+	url := "https://bingpay.ng/api/v1/purchase-bill"
+	method := "POST"
+	token := client.Token
+
+	payload := purchaseBill{}
+	payload.Service_Id = service_Id
+	payload.Customer_Id = customer_Id
+	payload.Variation = variation
+	payload.Amount = amount
+
+	jsonReq, jsonErr := json.Marshal(&payload)
+	if jsonErr != nil {
+		return nil, 0, jsonErr
+	}
+
+	req, reqErr := http.NewRequest(method, url, bytes.NewBuffer(jsonReq))
+	if reqErr != nil {
+		return nil, 0, reqErr
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", token)
+
+	resp, respErr := client.Http.Do(req)
+	if respErr != nil {
+		return nil, 0, respErr
+	}
+
+	defer resp.Body.Close()
+
+	resp_body, _ := ioutil.ReadAll(resp.Body)
+	var response purchaseBillRes
+	if err := json.Unmarshal(resp_body, &response); err != nil {
+		return nil, 0, err
+	}
+
+	return &response, resp.StatusCode, nil
+}
