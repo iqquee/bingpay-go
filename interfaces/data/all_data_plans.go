d3bbeeb6a0f45af3c679c5b8b1eac5c7d359b563
hisyntax <hisyntax0@gmail.com>
2022-06-27 02:18:04 +0100

    all data plans list implemented

---
+++ interfaces/data/all_data_plans.go
@@ -0,0 +1,52 @@
+package data
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/hisyntax/bingpay-go/interfaces"
+)
+
+type allDataPlans struct {
+	Error   bool
+	Message string
+	Data    []allDataPlansDataBody
+}
+
+type allDataPlansDataBody struct {
+	Id         string
+	Network_Id string
+	Name       string
+	Uniq_Id    string
+}
+
+func AllDataPlans() (*allDataPlans, int, error) {
+	client := interfaces.NewHttpClient()
+	url := "https://bingpay.ng/api/v1/all-data-plans"
+	method := "GET"
+	token := client.Token
+
+	req, reqErr := http.NewRequest(method, url, nil)
+	if reqErr != nil {
+		return nil, 0, reqErr
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", token)
+
+	resp, respErr := client.Http.Do(req)
+	if respErr != nil {
+		return nil, 0, respErr
+	}
+
+	defer resp.Body.Close()
+
+	resp_body, _ := ioutil.ReadAll(resp.Body)
+	var response allDataPlans
+	if err := json.Unmarshal(resp_body, &response); err != nil {
+		return nil, 0, err
+	}
+
+	return &response, resp.StatusCode, nil
+}
