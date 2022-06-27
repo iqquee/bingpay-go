e3e59cfeda38cd67d709b0275ccbd81fe708198d
hisyntax <hisyntax0@gmail.com>
2022-06-27 03:03:40 +0100

    get service variation implemented for utility payment

---
+++ interfaces/utilityBills/service_variation.go
@@ -0,0 +1,60 @@
+package utilitybills
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/hisyntax/bingpay-go/interfaces"
+)
+
+type serviceVariationRes struct {
+	Error   bool
+	Message string
+	Data    serviceVariationDataBody
+}
+
+type serviceVariationDataBody struct {
+	ServiceName     string
+	ServiceId       string
+	Convinience_Fee string
+	Variations      []variationsDataBody
+}
+
+type variationsDataBody struct {
+	Variation_Code   string
+	Name             string
+	Variation_Amount string
+	FixedPrice       string
+}
+
+func ServiceVariation(service_id int) (*serviceVariationRes, int, error) {
+	client := interfaces.NewHttpClient()
+	url := fmt.Sprintf("https://bingpay.ng/api/v1/service/%d", service_id)
+	method := "GET"
+	token := client.Token
+
+	req, reqErr := http.NewRequest(method, url, nil)
+	if reqErr != nil {
+		return nil, 0, reqErr
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", token)
+
+	resp, respErr := client.Http.Do(req)
+	if respErr != nil {
+		return nil, 0, respErr
+	}
+
+	defer resp.Body.Close()
+
+	resp_body, _ := ioutil.ReadAll(resp.Body)
+	var response serviceVariationRes
+	if err := json.Unmarshal(resp_body, &response); err != nil {
+		return nil, 0, err
+	}
+
+	return &response, resp.StatusCode, nil
+}
