ed8c117e19d77d531fd172772899e40431c3360a
hisyntax <hisyntax0@gmail.com>
2022-06-28 10:49:22 +0100

    get all banks implemented

---
+++ interfaces/bank/all_banks.go
@@ -0,0 +1,61 @@
+package bank
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/hisyntax/bingpay-go/interfaces"
+)
+
+type allBanksRes struct {
+	Error   bool
+	Message string
+	Data    []allBanksResDataBody
+}
+
+type allBanksResDataBody struct {
+	Name        string
+	Slug        string
+	Code        string
+	LongCode    string
+	Gateway     interface{}
+	PayWithBank bool
+	Active      bool
+	IsDeleted   bool
+	Country     string
+	Currency    string
+	Type        string
+	Id          int
+	CreatedAt   string
+	UpdatedAt   string
+}
+
+func AllBanks() (*allBanksRes, int, error) {
+	client := interfaces.NewHttpClient()
+	url := "https://bingpay.ng/api/v1/all-banks"
+	method := "POST"
+	token := client.Token
+	req, reqErr := http.NewRequest(method, url, nil)
+	if reqErr != nil {
+		return nil, 0, reqErr
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", token)
+
+	resp, respErr := client.Http.Do(req)
+	if respErr != nil {
+		return nil, 0, respErr
+	}
+
+	defer resp.Body.Close()
+
+	resp_body, _ := ioutil.ReadAll(resp.Body)
+	var response allBanksRes
+	if err := json.Unmarshal(resp_body, &response); err != nil {
+		return nil, 0, err
+	}
+
+	return &response, resp.StatusCode, nil
+}
